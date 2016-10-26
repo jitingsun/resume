c606dab2c9c9171992ba2d387a271f8f325271ec
jitingsun <846523689@qq.com>
2016-10-26 21:11:41 +0800

    Signed-off-by: jitingsun <846523689@qq.com>

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
+    direction: 'vertical',
     loop: false,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+    pagination : '.swiper-pagination',
+    paginationType:'progress',
     onSlideChangeEnd: function (swiper) {
         var slides = swiper.slides;//获取所有的滑块
         var curIndex = swiper.activeIndex;//当前活动的滑块的索引
@@ -21,8 +21,6 @@ var mySwiper = new Swiper('.swiper-container', {
                     default :
                         item.id = 'page' + curIndex;
                 }
-                // slides[index].id='page'+(index+1);
-
             }
         })
     },
@@ -31,22 +29,3 @@ var mySwiper = new Swiper('.swiper-container', {
 //            slides[0].id='page1';
     }
 });
-
-//var beyond = document.querySelector('#beyond');
-//var music = document.querySelector('.music');
-//window.setTimeout(function () {
-//    beyond.play();//音频文件播放 边缓冲边播放
-//    beyond.addEventListener('canplay', function () {
-//        music.className = 'music musicMove';
-//        music.style.opacity = 1;
-//    });
-//}, 1000);
-//music.addEventListener('click', function () {
-//    if(beyond.paused){
-//        beyond.play();
-//        music.className='music musicMove';
-//    }else{
-//        beyond.pause();
-//        music.className='music';
-//    }
-//})
