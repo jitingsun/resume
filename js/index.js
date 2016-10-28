4b1fb63b337725689e0bc1737a70f31d7ed37e80
jitingsun <846523689@qq.com>
2016-10-28 23:16:19 +0800

    Signed-off-by: jitingsun <846523689@qq.com>

--- js/index.js
+++ js/index.js
@@ -2,9 +2,6 @@
     var winW = document.documentElement.clientWidth;
     document.documentElement.style.fontSize = winW / desW * 100 + "px";
 }(320);
-/*划屏区域初始化*/
-
-
 var mySwiper = new Swiper('.swiper-container', {
     direction: "vertical",
     loop: true,
@@ -22,28 +19,3 @@ var mySwiper = new Swiper('.swiper-container', {
         }
     }
 });
-
-
-
-/*音频的自动播放*/
-~function () {
-    var audioBox=document.querySelector(".audio"),
-        myAudio=audioBox.getElementsByTagName("audio")[0];
-    window.setTimeout(function () {
-        myAudio.play();
-        myAudio.addEventListener("canplay",function () {
-            audioBox.style.display="block";
-            audioBox.className+=" audioMove";
-        },false);
-    },1000);
-    audioBox.addEventListener("click",function () {
-        if(myAudio.paused){
-            myAudio.play();
-            audioBox.className="audio audioMove";
-            return;
-        }
-        myAudio.pause();
-        audioBox.className="audio";
-    },false);
-
-}();
